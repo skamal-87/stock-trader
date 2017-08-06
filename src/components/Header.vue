14a30ee219e26ed0d0c4dd02a7248015f4146697
Steve Kamal <skamal@liveperson.com>
2017-08-06 19:56:08 -0400

    build placeholder components and setup routing. created stock card bootstrap containers

---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+    </div>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <router-link to="/portfolio" activeClass="active" tag="li"><a>Portfolio</a></router-link>
+        <router-link to="/stocks" activeClass="active" tag="li"><a>Stocks</a></router-link>
+      </ul>
+
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">End Day</a></li>
+        <li class="dropdown">
+          <a 
+          href="#" 
+          class="dropdown-toggle" 
+          data-toggle="dropdown" 
+          role="button" a
+          ria-haspopup="true" 
+          aria-expanded="false">Save & Load <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Save Data</a></li>
+            <li><a href="#">Load Data</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
