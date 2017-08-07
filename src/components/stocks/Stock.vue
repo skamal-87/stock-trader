65e4dcdf5d93ce6326e564f5c3eeac2988576abf
Steve Kamal <skamal@liveperson.com>
2017-08-06 20:22:45 -0400

    bind stock to stocks. add stock prop and use v-model to create order object for buy button

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -3,8 +3,8 @@
         <div class="panel panel-success">
             <div class="panel-heading">
                 <h3 class="panel-title">
-                    NAME
-                    <SMALL>(Price: PRICE)</small>
+                    {{ stock.name }}
+                    <SMALL>(Price: ${{ stock.price }})</small>
                 </h3>
             </div>
             <div class="panel-body">
@@ -12,11 +12,14 @@
                     <input 
                         type="number"
                         class="form-control"
-                        placeholder="Quantity">
+                        placeholder="Quantity"
+                        v-model="quantity">
                 </div>
                 <div class="pull-right">
                     <button
                         class="btn btn-success"
+                        @click="buyStock"
+                        :disabled="quantity <= 0"
                         >Buy</button>
                 </div>
             </div>
@@ -26,7 +29,23 @@
 
 <script>
 export default {
-
+    props: ['stock'],
+    data () {
+        return {
+            quantity: 0
+        }
+    },
+    methods: {
+        buyStock () {
+            const order = {
+                stockId: this.stock.id,
+                stockPrice: this.stock.price,
+                quantity: this.quantity
+            };
+            console.log(order);
+            this.quantity = 0;
+        }
+    }
 }
 </script>
 
